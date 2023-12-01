0607b3366e90a2f2fc1c8816806b8ae0b3c0ef6a
chowdary <chandrahari321@gmail.com>
2023-12-01 22:30:18 +0530

    automated commit #70

--- expense/data.tf
+++ expense/data.tf
@@ -17,5 +17,5 @@ data "aws_route53_zone" "zone" {
 
 locals {
   ami     = data.aws_ami.ami.image_id
-#  zone_id = data.aws_route53_zone.zone.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
 }
