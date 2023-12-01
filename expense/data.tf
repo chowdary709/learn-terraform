bf0b991f17828058a5aa3e49e97558a65e00fdc0
chowdary <chandrahari321@gmail.com>
2023-12-01 18:58:12 +0530

    automated commit #18

--- expense/data.tf
+++ expense/data.tf
@@ -1,11 +1,10 @@
-# data.aws_ami.ami.image_id
 
 data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
 }
 
 data "aws_security_group" "sg" {
-  id = "allow-all"  # This is causing the error
+  name = "allow-all"
 }
--- expense/main.tf
+++ expense/main.tf
@@ -1,25 +1,7 @@
-#resource "aws_instance" "frontend" {
-#  ami                    = data.aws_ami.ami.image_id
-#  instance_type          = "t2.micro"
-#  vpc_security_group_ids = [data.aws_security_group.sg.id]
-#
-#  tags = {
-#    Name = "frontend"
-#  }
-#}
-#
-#resource "aws_route53_record" "frontend" {
-#  name    = "frontend.roboshop.internal"
-#  type    = "A"
-#  zone_id = "Z08360431XA1BOY4SK2N0"
-#  ttl = "5"
-#  records = [ aws_instance.frontend.private_ip]
-#}
-
 resource "aws_instance" "frontend" {
-  ami = data.aws_ami.ami.image_id
+  ami           = data.aws_ami.ami.image_id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [data.aws_security_group.sg]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "frontend"
@@ -27,12 +9,11 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
+  zone_id = "Z08360431XA1BOY4SK2N0"
   name    = "frontend.roboshop.internal"
   type    = "A"
-  zone_id = "Z08360431XA1BOY4SK2N0"
-  ttl     = 5
-  records = [aws_instance.frontend.private_ip]
+  ttl     = 30
+  records = [ aws_instance.frontend.private_ip ]
 }
-
 
 
