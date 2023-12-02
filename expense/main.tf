52a53d2d3924a7894e1812d3690e01a5b4f6a8e3
chowdary <chandrahari321@gmail.com>
2023-12-02 19:04:31 +0530

    automated commit #94

--- expense/main.tf
+++ expense/main.tf
@@ -1,22 +1,20 @@
 resource "aws_instance" "frontend" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = "t2.micro"
+  ami                    = local.ami
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "frontend"
   }
-
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = data.aws_route53_zone.zone.zone_id
+  zone_id = local.zone_id
   name    = "frontend.${var.zone_name}"
   type    = "A"
   ttl     = 30
-  records = [ aws_instance.frontend.private_ip ]
+  records = [aws_instance.frontend.private_ip]
 }
-
 resource "null_resource" "frontend" {
   depends_on = [aws_route53_record.frontend]
 
@@ -25,7 +23,7 @@ resource "null_resource" "frontend" {
 cd /home/centos/infra-ansible
 git pull
 sleep 60
-ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+ansible-playbook -i ${aws_instance.frontend.private_ip}, expense.yml -e role_name=frontend
 EOF
   }
 }
