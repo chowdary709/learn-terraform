481c5cdf20af5a24992de95443c549cc1661fc14
chowdary <chandrahari321@gmail.com>
2023-12-02 19:02:14 +0530

    automated commit #92

--- expense/main.tf
+++ expense/main.tf
@@ -1,26 +1,31 @@
 resource "aws_instance" "frontend" {
-  ami                    = local.ami
-  instance_type          = "t2.micro"
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "frontend"
   }
-  provisioner "local-exec" {
-    command = <<EOF
-cd /root/infra-ansible/roles
-git pull
-sleep 60
-/path/to/ansible-playbook -i ${aws_instance.frontend.private_ip}, expense.yml -e role_name=frontend
-EOF
-  }
 
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = local.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "frontend.${var.zone_name}"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = [ aws_instance.frontend.private_ip ]
+}
+
+resource "null_resource" "frontend" {
+  depends_on = [aws_route53_record.frontend]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/infra-ansible
+git pull
+sleep 60
+ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+EOF
+  }
 }
