54c687fa09fdf329d652341a98ddcbeb3db69f39
chowdary <chandrahari321@gmail.com>
2023-12-02 18:32:46 +0530

    automated commit #88

--- expense/main.tf
+++ expense/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "frontend" {
 cd /home/chandrahari321/infra-ansible
 git pull
 sleep 60
-ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+ansible-playbook -i ${aws_instance.frontend.private_ip}, expense.yml -e role_name=frontend
 EOF
   }
 }
