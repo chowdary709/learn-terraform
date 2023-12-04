1e417517a2dd2b165b6ddfbbcaf4c3c01a255573
chowdary <chandrahari321@gmail.com>
2023-12-04 18:10:45 +0530

    automated commit #139

--- expense-v1/module/data.tf
+++ expense-v1/module/data.tf
@@ -1,22 +1,46 @@
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#  owners      = ["973714476881"]
+#}
+#
+#
+#data "aws_security_group" "sg" {
+#  name = "allow-all"
+#}
+#
+#data "aws_route53_zone" "zone" {
+#  name = var.zone_name
+#  private_zone  = true
+#}
+#
+#variable "zone_name" {
+#  default = "roboshop.internal"
+#}
+#
+#locals {
+#  ami     = data.aws_ami.ami.image_id
+#  zone_id = data.aws_route53_zone.zone.zone_id
+#}
+
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
 
-
 data "aws_security_group" "sg" {
   name = "allow-all"
 }
 
 data "aws_route53_zone" "zone" {
-  name = var.zone_name
-  private_zone  = true
+  name = var.zone_id
 }
 
-variable "zone_name" {
+variable "zone_id" {
   default = "roboshop.internal"
 }
+
 
 locals {
   ami     = data.aws_ami.ami.image_id
--- expense-v1/module/main.tf
+++ expense-v1/module/main.tf
@@ -1,16 +1,49 @@
+#resource "aws_instance" "instance" {
+#  ami                    = local.ami
+#  instance_type          = "t2.micro"
+#  vpc_security_group_ids = [data.aws_security_group.sg.id]
+#
+#  tags = {
+#    Name = var.component
+#  }
+#}
+#
+#resource "aws_route53_record" "record" {
+#  zone_id = local.zone_id
+#  name    = "${var.component}.${var.zone_name}"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.instance.private_ip]
+#}
+#
+#resource "null_resource" "ansible" {
+#  depends_on = [aws_route53_record.record]
+#
+#
+#  provisioner "local-exec" {
+#    command = <<EOF
+#cd /root/expense-ansible/roles
+#git pull
+#sleep 60
+#ansible-playbook -i ${aws_instance.instance.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 expense.yml -e role_name=${var.component}
+#EOF
+#  }
+#}
+
 resource "aws_instance" "instance" {
   ami                    = local.ami
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = var.component
   }
+
 }
 
 resource "aws_route53_record" "record" {
   zone_id = local.zone_id
-  name    = "${var.component}.${var.zone_name}"
+  name    = "${var.component}.${var.zone_id}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
@@ -18,7 +51,6 @@ resource "aws_route53_record" "record" {
 
 resource "null_resource" "ansible" {
   depends_on = [aws_route53_record.record]
-
 
   provisioner "local-exec" {
     command = <<EOF
@@ -29,20 +61,3 @@ ansible-playbook -i ${aws_instance.instance.private_ip}, -e ansible_user=centos 
 EOF
   }
 }
-
-#╷
-#│ Error: creating Route 53 Record: InvalidChangeBatch: [Tried to create resource record set [name='mysql.roboshop.internal.', type='A'] but it already exists]
-#│       status code: 400, request id: e076580c-26fb-4c7f-8a67-66e55474d904
-#│
-#│   with module.expense[1].aws_route53_record.record,
-#│   on module/main.tf line 11, in resource "aws_route53_record" "record":
-#│   11: resource "aws_route53_record" "record" {
-#│
-#╵
-#╷
-#│ Error: creating Route 53 Record: InvalidChangeBatch: [Tried to create resource record set [name='backend.roboshop.internal.', type='A'] but it already exists]
-#│       status code: 400, request id: e75fca39-8c1a-4bc1-8d93-6e56b90b30dd
-#│
-#│   with module.expense[2].aws_route53_record.record,
-#│   on module/main.tf line 11, in resource "aws_route53_record" "record":
-#│   11: resource "aws_route53_record" "record" {
