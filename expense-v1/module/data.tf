74648fc2160191792dacc56e08df3d0df8ac0ff1
chowdary <chandrahari321@gmail.com>
2023-12-04 18:11:33 +0530

    automated commit #140

--- expense-v1/module/data.tf
+++ expense-v1/module/data.tf
@@ -1,46 +1,22 @@
-#data "aws_ami" "ami" {
-#  most_recent = true
-#  name_regex  = "Centos-8-DevOps-Practice"
-#  owners      = ["973714476881"]
-#}
-#
-#
-#data "aws_security_group" "sg" {
-#  name = "allow-all"
-#}
-#
-#data "aws_route53_zone" "zone" {
-#  name = var.zone_name
-#  private_zone  = true
-#}
-#
-#variable "zone_name" {
-#  default = "roboshop.internal"
-#}
-#
-#locals {
-#  ami     = data.aws_ami.ami.image_id
-#  zone_id = data.aws_route53_zone.zone.zone_id
-#}
-
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
 
+
 data "aws_security_group" "sg" {
   name = "allow-all"
 }
 
 data "aws_route53_zone" "zone" {
-  name = var.zone_id
+  name = var.zone_name
+  private_zone  = true
 }
 
-variable "zone_id" {
+variable "zone_name" {
   default = "roboshop.internal"
 }
-
 
 locals {
   ami     = data.aws_ami.ami.image_id
--- expense-v1/module/main.tf
+++ expense-v1/module/main.tf
@@ -1,49 +1,16 @@
-#resource "aws_instance" "instance" {
-#  ami                    = local.ami
-#  instance_type          = "t2.micro"
-#  vpc_security_group_ids = [data.aws_security_group.sg.id]
-#
-#  tags = {
-#    Name = var.component
-#  }
-#}
-#
-#resource "aws_route53_record" "record" {
-#  zone_id = local.zone_id
-#  name    = "${var.component}.${var.zone_name}"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.instance.private_ip]
-#}
-#
-#resource "null_resource" "ansible" {
-#  depends_on = [aws_route53_record.record]
-#
-#
-#  provisioner "local-exec" {
-#    command = <<EOF
-#cd /root/expense-ansible/roles
-#git pull
-#sleep 60
-#ansible-playbook -i ${aws_instance.instance.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 expense.yml -e role_name=${var.component}
-#EOF
-#  }
-#}
-
 resource "aws_instance" "instance" {
   ami                    = local.ami
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = var.component
   }
-
 }
 
 resource "aws_route53_record" "record" {
   zone_id = local.zone_id
-  name    = "${var.component}.${var.zone_id}"
+  name    = "${var.component}.${var.zone_name}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
@@ -51,6 +18,7 @@ resource "aws_route53_record" "record" {
 
 resource "null_resource" "ansible" {
   depends_on = [aws_route53_record.record]
+
 
   provisioner "local-exec" {
     command = <<EOF
@@ -61,3 +29,4 @@ ansible-playbook -i ${aws_instance.instance.private_ip}, -e ansible_user=centos 
 EOF
   }
 }
+
